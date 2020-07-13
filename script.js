c88c3e4f7115669f5e76b2322e636572ca72fdc6
Владимир Иванович Сотников <39740656+Sotnikov-Vladimir@users.noreply.github.com>
2020-07-13 19:04:13 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 'use strict';
 let x = prompt("Enter x:");
 let n = prompt("Enter n:");
-pow(x,n);
+console.log(pow(x,n));
 function pow(x,n){
     let result = x;
     for (let i = 1; i < n; i++){
         result *= x;
     }
     return result;
-    console.log(result);
 }
